2365d64bdd0c4db9f0b40e59bb9fb9099d959c73
Lizzette24 <Lizzette24>
2022-05-15 07:36:12 -0500

    Proyecto Mapbox

---
+++ mapbox_angular_8_13/src/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MapaComponent } from './app/components/mapa/mapa.component';
+import { HttpClientModule } from '@angular/common/http';
+import { SocketIoModule } from 'ngx-socket-io';
+import { environment } from 'src/environments/environment';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapaComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    SocketIoModule.forRoot(environment.socketConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
